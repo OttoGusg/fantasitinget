ec5e0cb268cd22cb0a4260916367743d98be7217
MatEide <matheide@outlook.com>
2023-01-26 11:30:09 +0100

    Fortsettelse på spiller

--- spiller.py
+++ spiller.py
@@ -1,1 +1,25 @@
-class
+class Spiller:
+    def __init__(self, brukernavn):
+        self.brukernavn = brukernavn
+        self.stemmer = 100 
+        self.politikere = []
+        self.poengsum = 0
+
+def kjøp_politiker(self, ny_politiker):
+    self.politikere.append(ny_politiker)
+    self.poengsum += ny_politiker.hent_poengsum()
+    if ny_politiker.hent_pris() > self.stemmer:
+        return f'Du har ikke nok stemmer til {ny_politiker}'
+    else:
+        self.stemmer -= ny_politiker.hent_pris()
+        return f'Gratulerer du har kjøpt {ny_politiker}!!!'
+
+def hent_politikere(self):
+    return self._politikere
+
+def selg_politiker(self):
+    pass
+
+def beregn_poengsum(self):
+    return self.poengsum
+        
