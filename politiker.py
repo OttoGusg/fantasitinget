1d81e8479ad5307dbb2cef9cfd0ffc8d85a856dd
OttoGusg <otto@gusgaard.com>
2023-01-26 10:39:33 +0100

    lagde politiker oppsett helt ferdig
    Co-Authored-By: MatEide <122024455+MatEide@users.noreply.github.com>

--- politiker.py
+++ politiker.py
@@ -1,4 +1,15 @@
 class Politiker:
-    def __init__(self):
-        pass
+    def __init__(self, stemmeprosent:int, regjering:True or False, pris:int,poengsum:int, saker:list):
+        self.stemmeprosent = stemmeprosent
+        self.regjering = regjering
+        self.pris = pris
+        self.poengsum = poengsum
+        self.saker = []
         
+    def sak_sukess(self):
+        pass
+
+    def sak_fiasko( self):
+        pass
+
+
